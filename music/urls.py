eff5536f2da52021ca0acb42fab0e8f8c0bda92c
Ash96win-dev <67545562+Ash96win-dev@users.noreply.github.com>
2020-06-28 22:41:07 +0530

    Added a new view

--- music/urls.py
+++ music/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    #/music/
     url(r'^$', views.index, name='index'),
+
+    #/music/71/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- music/views.py
+++ music/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<H1>This is the Music home page</H1>")
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id :" + str(album_id)+"</h2>")
