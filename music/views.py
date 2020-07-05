221f44e16e0c0aa899155651cfb18aa92bb1da3b
Ash96win-dev <67545562+Ash96win-dev@users.noreply.github.com>
2020-07-05 19:07:30 +0530

    added new html

--- music/views.py
+++ music/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Album
+from django.template import loader
 def index(request):
-    return HttpResponse("<H1>This is the Music home page</H1>")
+
+    all_albums = Album.objects.all()
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums' : all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id :" + str(album_id)+"</h2>")
+    template = loader.get_template('music/albumid.html')
+    context = {
+        'all_albums': album_id,
+    }
+    return HttpResponse(template.render(context, request))
